95a5802620a95d49352b6a4c2b718efb78acd906
MissJuliaRobot <54566209+MissJuliaRobot@users.noreply.github.com>
2020-11-19 21:08:02 +0530

    Update Utilities_⚡.py

--- julia/modules/Utilities_⚡.py
+++ julia/modules/Utilities_⚡.py
@@ -275,7 +275,7 @@ async def ping(event):
     await message.edit("Pinging ...")
     end_time = datetime.datetime.now()
     pingtime = end_time - start_time
-    telegram_ping = str(pingtime.total_seconds()) +"s"    
+    telegram_ping = str(round(pingtime.total_seconds(), 2)) +"s"    
     uptime = get_readable_time((time.time() - StartTime))
     await message.edit(
         "PONG !\n"
