538e9f518352f81369a4bea9f8422938e48efd02
MissJuliaRobot <54566209+MissJuliaRobot@users.noreply.github.com>
2020-11-20 17:31:44 +0530

    Update Stickers_🦎.py

--- julia/modules/Stickers_🦎.py
+++ julia/modules/Stickers_🦎.py
@@ -7,12 +7,51 @@ from telethon.tl.types import (
 )
 
 from julia.events import register
+from telethon import *
+from telethon import events
+from telethon.tl import functions
+from telethon.tl import types
+from pymongo import MongoClient
 
+client = MongoClient()
+client = MongoClient(MONGO_DB_URI)
+db = client["missjuliarobot"]
+approved_users = db.approve
+
+async def is_register_admin(chat, user):
+    if isinstance(chat, (types.InputPeerChannel, types.InputChannel)):
+
+        return isinstance(
+            (await
+             tbot(functions.channels.GetParticipantRequest(chat,
+                                                           user))).participant,
+            (types.ChannelParticipantAdmin, types.ChannelParticipantCreator),
+        )
+    if isinstance(chat, types.InputPeerChat):
+
+        ui = await tbot.get_peer_id(user)
+        ps = (await tbot(functions.messages.GetFullChatRequest(chat.chat_id)
+                         )).full_chat.participants.participants
+        return isinstance(
+            next((p for p in ps if p.user_id == ui), None),
+            (types.ChatParticipantAdmin, types.ChatParticipantCreator),
+        )
+    return None
 
 @register(pattern="^/packinfo$")
 async def _(event):
-    if event.fwd_from:
-        return
+    approved_userss = approved_users.find({})
+    for ch in approved_userss:
+        iid = ch["id"]
+        userss = ch["user"]
+    if event.is_group:
+        if (await is_register_admin(event.input_chat, event.message.sender_id)):
+            pass
+        elif event.chat_id == iid and event.sender_id == userss:
+            pass
+        else:
+            return
+  
     if not event.is_reply:
         await event.reply("Reply to any sticker to get it's pack info.")
         return
@@ -78,8 +117,17 @@ FILLED_UP_DADDY = "Invalid pack selected."
 
 @register(pattern="^/kang ?(.*)")
 async def _(event):
-    if event.fwd_from:
-        return
+    approved_userss = approved_users.find({})
+    for ch in approved_userss:
+        iid = ch["id"]
+        userss = ch["user"]
+    if event.is_group:
+        if (await is_register_admin(event.input_chat, event.message.sender_id)):
+            pass
+        elif event.chat_id == iid and event.sender_id == userss:
+            pass
+        else:
+            return
     if not event.is_reply:
         await event.reply("Reply to a photo to add to my personal sticker pack.")
         return
