fb4f39ba9b7fe1414cd3e13e80dd0a307245de52
MissJuliaRobot <54566209+MissJuliaRobot@users.noreply.github.com>
2020-11-20 21:46:48 +0530

    Update events.py

--- julia/events.py
+++ julia/events.py
@@ -27,26 +27,20 @@ client = MongoClient(MONGO_DB_URI)
 dbb = client["missjuliarobot"]
 leechers = dbb.leecher
 
-logging.basicConfig(
-    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
-    level=logging.INFO)
-
 
 def register(**args):
-    """ Registers a new message. """
+    logging.basicConfig(
+     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+     level=logging.INFO)
     pattern = args.get('pattern')
-
     r_pattern = r'^[/]'
     if pattern is not None and not pattern.startswith('(?i)'):
         args['pattern'] = '(?i)' + pattern
     args['pattern'] = pattern.replace('^/', r_pattern, 1)
-
     stack = inspect.stack()
     previous_stack_frame = stack[1]
     file_test = Path(previous_stack_frame.filename)
-    file_test = file_test.stem.replace(".py", "")
-    
-
+    file_test = file_test.stem.replace(".py", "")   
     reg = re.compile("(.*)")
     if not pattern == None:
         try:
@@ -103,7 +97,9 @@ def register(**args):
     return decorator
 
 def juliabot(**args):
-    """ Register a new event. """
+    logging.basicConfig(
+     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+     level=logging.INFO)
     pattern = args.get('pattern', None)
     disable_edited = args.get('disable_edited', False)
     ignore_unsafe = args.get('ignore_unsafe', False)
