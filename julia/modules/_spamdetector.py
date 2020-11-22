17704d7f0edc15dfd78d93a0b4acf84aad75e9bb
MissJuliaRobot <54566209+MissJuliaRobot@users.noreply.github.com>
2020-11-22 22:14:51 +0530

    Update _spamdetector.py

--- julia/modules/_spamdetector.py
+++ julia/modules/_spamdetector.py
@@ -12,47 +12,53 @@ from telethon.tl import functions
 # MADE BY @MISSJULIA_ROBOT
 
 global spamcounter
-spamcounter=0
+spamcounter = 0
+
 
 @tbot.on(events.NewMessage(pattern=None))
 async def leechers(event):
     if str(event.sender_id) in str(OWNER_ID):
-      return
+        return
     global spamcounter
     global starttimer
-    starttimer=time.time()   
+    starttimer = time.time()
     spamcounter += 1
     sender = event.sender_id
     senderr = await event.get_sender()
+    check = sender
     USERSPAM = []
     if len(USERSPAM) >= 1:
         if event.sender_id == USERSPAM[0]:
             pass
         else:
             USERSPAM = []
-            USERSPAM.append(check)  
+            USERSPAM.append(check)
     else:
         USERSPAM = []
-        USERSPAM.append(check)  
-    
-    if spamcounter > 4 :
-       spamtimecheck = time.time() - starttimer 
-    if spamcounter > 4 and event.sender_id == USERSPAM[0] and (time.strftime("%S", time.gmtime(spamtimecheck))) <= "03":
-            VALID = True
-            spamcounter = 0
-            if senderr.username == None:
-                st = senderr.first_name
-                hh = senderr.id
-                final = f"[{st}](tg://user?id={hh}) you are detected as a spammer according to my algorithms.\nYou will be restricted from using any bot commands for 24 hours !"
-            else:
-                st = senderr.username
-                final = f"@{st} you are detected as a spammer according to my algorithms.\nYou will be restricted from using any bot commands for 24 hours !"         
-                pass  
+        USERSPAM.append(check)
+
+    if spamcounter > 4:
+        spamtimecheck = time.time() - starttimer
+    if (
+        spamcounter > 4
+        and event.sender_id == USERSPAM[0]
+        and (time.strftime("%S", time.gmtime(spamtimecheck))) <= "03"
+    ):
+        VALID = True
+        spamcounter = 0
+        if senderr.username == None:
+            st = senderr.first_name
+            hh = senderr.id
+            final = f"[{st}](tg://user?id={hh}) you are detected as a spammer according to my algorithms.\nYou will be restricted from using any bot commands for 24 hours !"
+        else:
+            st = senderr.username
+            final = f"@{st} you are detected as a spammer according to my algorithms.\nYou will be restricted from using any bot commands for 24 hours !"
+            pass
     else:
-            VALID = False
-            del spamtimecheck 
-            del spamcounter 
-            del starttimer 
+        VALID = False
+        del spamtimecheck
+        del spamcounter
+        del starttimer
 
     dev = await event.respond(final)
 
@@ -62,17 +68,17 @@ async def leechers(event):
 
     users = leechers.find({})
     for c in users:
-      if USERSPAM[0] == c["id"]:
-         return
-      timerr = time.time()
-      leechers.insert_one({"id": USERSPAM[0], "time": timerr})
-      try:
-         MUTE_RIGHTS = ChatBannedRights(until_date=None, send_messages=True)
-         await tbot(EditBannedRequest(event.chat_id, event.sender_id, MUTE_RIGHTS))
-         await dev.edit(final + "\nYou are now muted !")
-      except Exception:
-             pass 
+        if USERSPAM[0] == c["id"]:
+            return
+        timerr = time.time()
+        leechers.insert_one({"id": USERSPAM[0], "time": timerr})
+        try:
+            MUTE_RIGHTS = ChatBannedRights(until_date=None, send_messages=True)
+            await tbot(EditBannedRequest(event.chat_id, event.sender_id, MUTE_RIGHTS))
+            await dev.edit(final + "\nYou are now muted !")
+        except Exception:
+            pass
 
-      del spamtimecheck 
-      del spamcounter 
-      del starttimer
+        del spamtimecheck
+        del spamcounter
+        del starttimer
