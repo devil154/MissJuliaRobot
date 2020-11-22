1d6d39c603a73b5598af0625cc5bb7456aacf230
MissJuliaRobot <54566209+MissJuliaRobot@users.noreply.github.com>
2020-11-22 21:32:50 +0530

    Update and rename Anti-Spam_😈 to _spamdetector.py

--- julia/modules/_spamdetector.py
+++ julia/modules/_spamdetector.py
@@ -44,7 +44,7 @@ async def leechers(event):
     
     if spamcounter > 4 and event.sender_id == USERSPAM[0]:
      spamtimecheck = time.time() - starttimer
-     if str(time.strftime("%S", time.gmtime(spamtimecheck))) <= "03": 
+     if (time.strftime("%S", time.gmtime(spamtimecheck))) <= "03": 
             print(time.strftime("%S", time.gmtime(spamtimecheck)))
             VALID = True
             spamcounter = 0
