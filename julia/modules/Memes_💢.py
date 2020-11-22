44b32bb53d2008d85c75fd48f84ca06324b41657
MissJuliaRobot <54566209+MissJuliaRobot@users.noreply.github.com>
2020-11-22 19:49:08 +0530

    Update Memes_💢.py

--- julia/modules/Memes_💢.py
+++ julia/modules/Memes_💢.py
@@ -440,34 +440,6 @@ from julia import GOOGLE_CHROME_BIN
 
 CARBONLANG = "en"
 
-client = MongoClient()
-client = MongoClient(MONGO_DB_URI)
-db = client["missjuliarobot"]
-approved_users = db.approve
-
-
-async def is_register_admin(chat, user):
-    if isinstance(chat, (types.InputPeerChannel, types.InputChannel)):
-
-        return isinstance(
-            (
-                await tbot(functions.channels.GetParticipantRequest(chat, user))
-            ).participant,
-            (types.ChannelParticipantAdmin, types.ChannelParticipantCreator),
-        )
-    if isinstance(chat, types.InputPeerChat):
-
-        ui = await tbot.get_peer_id(user)
-        ps = (
-            await tbot(functions.messages.GetFullChatRequest(chat.chat_id))
-        ).full_chat.participants.participants
-        return isinstance(
-            next((p for p in ps if p.user_id == ui), None),
-            (types.ChatParticipantAdmin, types.ChatParticipantCreator),
-        )
-    return None 
-
-
 @register(pattern="^/carbon (.*)")
 async def carbon_api(e):
     """this method of approve system is made by @AyushChatterjee, god will curse your family if you kang it motherfucker"""
@@ -532,15 +504,11 @@ async def carbon_api(e):
     )
     os.remove("./carbon.png")
     driver.quit()
+
 from random import randint, uniform
 
 from PIL import ImageEnhance, ImageOps
 from telethon.tl.types import DocumentAttributeFilename
-
-client = MongoClient()
-client = MongoClient(MONGO_DB_URI)
-db = client["missjuliarobot"]
-approved_users = db.approve
 
 @register(pattern="^/deepfry(?: |$)(.*)")
 async def deepfryer(event):
@@ -641,12 +609,6 @@ async def check_media(reply_message):
         return False
     else:
         return data
-
-client = MongoClient()
-client = MongoClient(MONGO_DB_URI)
-db = client["missjuliarobot"]
-approved_users = db.approve
-
 
 @register(pattern="^/type (.*)")
 async def typewriter(typew):
