e04065eaca4a04f7e46a3c495ecae514cec49051
MissJuliaRobot <54566209+MissJuliaRobot@users.noreply.github.com>
2020-11-23 09:40:44 +0530

    Update Song_🎵.py

--- julia/modules/Song_🎵.py
+++ julia/modules/Song_🎵.py
@@ -1,3 +1,33 @@
+# Created by @p_rinc_e
+from pathlib import Path
+import asyncio, time, io, math, os, logging, asyncio, shutil, re, subprocess, json
+from re import findall
+from asyncio import sleep
+from telethon.events import NewMessage
+from telethon.tl.custom import Dialog
+from datetime import datetime as dt
+from pytz import country_names as c_n, country_timezones as c_tz, timezone as tz
+from hachoir.parser import createParser
+import pybase64
+from base64 import b64decode
+from pySmartDL import SmartDL
+from telethon.tl.types import DocumentAttributeVideo, DocumentAttributeAudio
+from telethon import events
+from telethon.errors.rpcerrorlist import YouBlockedUserError
+from telethon.tl.functions.messages import ImportChatInviteRequest as Get
+from validators.url import url
+from html import unescape
+from urllib.error import HTTPError
+import bs4
+from bs4 import BeautifulSoup
+from youtube_dl import YoutubeDL
+
+from youtube_dl.utils import (DownloadError, ContentTooShortError,
+
+                              ExtractorError, GeoRestrictedError,
+                              MaxDownloadsReached, PostProcessingError,
+                              UnavailableVideoError, XAttrMetadataError)
+
 from julia import tbot
 import glob
 import os
@@ -39,47 +69,101 @@ async def is_register_admin(chat, user):
 
 
 @register(pattern="^/song (.*)")
-async def _(event):
-    if event.fwd_from:
-        return
-    """this method of approve system is made by @AyushChatterjee, god will curse your family if you kang it motherfucker"""
-    approved_userss = approved_users.find({})
-    for ch in approved_userss:
-        iid = ch["id"]
-        userss = ch["user"]
-    if event.is_group:
-        if (await is_register_admin(event.input_chat, event.message.sender_id)):
-            pass
-        elif event.chat_id == iid and event.sender_id == userss:
-            pass
-        else:
-            return
-
-    cmd = event.pattern_match.group(1)
-    cmnd = f"{cmd}"
-    reply_to_id = event.message.id
-    if event.reply_to_msg_id:
-        reply_to_id = event.reply_to_msg_id
+async def download_video(v_url):
+    url = v_url.pattern_match.group(1)
+    rkp = await rkp.reply("`Processing ...`")
+    if not url:
+       await rkp.edit("`Error \nusage song <song name>`")
+    search = SearchVideos(url, offset = 1, mode = "json", max_results = 1)
+    test = search.result()
+    p = json.loads(test)
+    q = p.get('search_result')
     try:
-        subprocess.run(["spotdl", cmnd])
-        subprocess.run(
-            'for f in *.opus; do      mv -- "$f" "${f%.opus}.mp3"; done', shell=True
+       url = q[0]['link']
+    except:
+    	return await rkp.edit("`Failed to find that song`")
+    type = "audio"
+    await rkp.edit("`Preparing to download...`")
+    if type == "audio":
+        opts = {
+            'format':
+            'bestaudio',
+            'addmetadata':
+            True,
+            'key':
+            'FFmpegMetadata',
+            'writethumbnail':
+            True,
+            'prefer_ffmpeg':
+            True,
+            'geo_bypass':
+            True,
+            'nocheckcertificate':
+            True,
+            'postprocessors': [{
+                'key': 'FFmpegExtractAudio',
+                'preferredcodec': 'mp3',
+                'preferredquality': '320',
+            }],
+            'outtmpl':
+            '%(id)s.mp3',
+            'quiet':
+            True,
+            'logtostderr':
+            False
+        }
+        video = False
+        song = True    
+    try:
+        await rkp.edit("`Fetching data, please wait..`")
+        with YoutubeDL(opts) as rip:
+            rip_data = rip.extract_info(url)
+    except DownloadError as DE:
+        await rkp.edit(f"`{str(DE)}`")
+        return
+    except ContentTooShortError:
+        await rkp.edit("`The download content was too short.`")
+        return
+    except GeoRestrictedError:
+        await rkp.edit(
+            "`Video is not available from your geographic location due to geographic restrictions imposed by a website.`"
         )
-        l = glob.glob("*.mp3")
-        loa = l[0]
-        await event.reply("sending the song")
-        await tbot.send_file(
-            event.chat_id,
-            loa,
-            force_document=False,
-            allow_cache=False,
+        return
+    except MaxDownloadsReached:
+        await rkp.edit("`Max-downloads limit has been reached.`")
+        return
+    except PostProcessingError:
+        await rkp.edit("`There was an error during post processing.`")
+        return
+    except UnavailableVideoError:
+        await rkp.edit("`Media is not available in the requested format.`")
+        return
+    except XAttrMetadataError as XAME:
+        await rkp.edit(f"`{XAME.code}: {XAME.msg}\n{XAME.reason}`")
+        return
+    except ExtractorError:
+        await rkp.edit("`There was an error during info extraction.`")
+        return
+    except Exception as e:
+        await rkp.edit(f"{str(type(e)): {str(e)}}")
+        return
+    c_time = time.time()
+    if song:
+        await rkp.edit(f"`Preparing to upload song:`\
+        \n**{rip_data['title']}**\
+        \nby **{rip_data['uploader']}**")
+        await v_url.client.send_file(
+            v_url.chat_id,
+            f"{rip_data['id']}.mp3",
             supports_streaming=True,
-            caption=cmd,
-            reply_to=reply_to_id,
-        )
-        os.system("rm -rf *.mp3")
-    except Exception:
-        await event.reply("I am getting too many requests !\nPlease try again later.")
+            attributes=[
+                DocumentAttributeAudio(duration=int(rip_data['duration']),
+                                       title=str(rip_data['title']),
+                                       performer=str(rip_data['uploader']))
+            ]    
+        os.remove(f"{rip_data['id']}.mp3")
+    
+
 from julia import CMD_HELP
 global __help__
 global file_helpo
