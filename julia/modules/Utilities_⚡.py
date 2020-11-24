54dd13c1c9b45518ad520d55724535be62709a7e
MissJuliaRobot <ayushchatterjee2003@gmail.com>
2020-11-24 18:51:36 +0530

    Update Utilities_⚡.py

--- julia/modules/Utilities_⚡.py
+++ julia/modules/Utilities_⚡.py
@@ -130,12 +130,6 @@ async def fetch_info(replied_user, event):
         "This User has no Username")
     user_bio = "This User has no About" if not user_bio else user_bio
 
-    if user_id != (await tbot.get_me()).id:
-        common_chat = replied_user.common_chats_count
-    else:
-        common_chat = "I've seen them in... Wow. Are they stalking me? "
-        common_chat += "They're in all the same places I am... oh. It's me."
-
     caption = "<b>USER INFO:</b> \n"
     caption += f"First Name: {first_name} \n"
     caption += f"Last Name: {last_name} \n"
@@ -145,7 +139,7 @@ async def fetch_info(replied_user, event):
     caption += f"Is Verified by Telegram: {verified} \n"
     caption += f"ID: <code>{user_id}</code> \n \n"
     caption += f"Bio: \n<code>{user_bio}</code> \n \n"
-    caption += f"Common Chats with this user: {common_chat} \n\n"
+    # caption += f"Common Chats with this user: {common_chat} \n\n"
     caption += f"Permanent Link To Profile: "
     caption += f"<a href=\"tg://user?id={user_id}\">{first_name}</a>"
 
