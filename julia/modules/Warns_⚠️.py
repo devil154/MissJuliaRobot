a0b2b5da2d1b5c0fd04df9df196cee0ba08bb043
MissJuliaRobot <ayushchatterjee2003@gmail.com>
2020-11-24 18:08:55 +0530

    Update Warns_⚠️.py

--- julia/modules/Warns_⚠️.py
+++ julia/modules/Warns_⚠️.py
@@ -1,11 +1,40 @@
 import asyncio
 import html
 from telethon import *
+from telethon.tl import *
 from telethon.tl.functions.channels import EditBannedRequest
 from telethon.tl.types import ChatBannedRights
 from julia.events import register
 import julia.modules.sql.warns_sql as sql
+from pymongo import MongoClient
+from julia import MONGO_DB_URI
 
+client = MongoClient()
+client = MongoClient(MONGO_DB_URI)
+db = client["missjuliarobot"]
+approved_users = db.approve
+
+
+async def is_register_admin(chat, user):
+    if isinstance(chat, (types.InputPeerChannel, types.InputChannel)):
+
+        return isinstance(
+            (
+                await tbot(functions.channels.GetParticipantRequest(chat, user))
+            ).participant,
+            (types.ChannelParticipantAdmin, types.ChannelParticipantCreator),
+        )
+    if isinstance(chat, types.InputPeerChat):
+
+        ui = await tbot.get_peer_id(user)
+        ps = (
+            await tbot(functions.messages.GetFullChatRequest(chat.chat_id))
+        ).full_chat.participants.participants
+        return isinstance(
+            next((p for p in ps if p.user_id == ui), None),
+            (types.ChatParticipantAdmin, types.ChatParticipantCreator),
+        )
+    return None
 
 @register(pattern="^/warn")
 async def _(event):
